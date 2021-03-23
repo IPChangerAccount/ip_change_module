fb1424b387f8a7dcbe659c5ed41eb07c04e33197
Liciliy <vladyslav.daniliants@gmail.com>
2021-03-23 15:38:37 +0200

    Notes added.

--- usage_example.py
+++ usage_example.py
@@ -1,3 +1,7 @@
+# NOTES:
+# 1) To use the IP change function import func change_ip from ip_change_module
+# 2) The script that will use the module must run as administrator.
+
 from ip_change_module import change_ip
 
 
